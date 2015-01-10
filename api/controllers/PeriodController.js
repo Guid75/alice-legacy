faae626857be52aa59f272168ba9493e4c0947e9
Guillaume Denry <guillaume.denry@gmail.com>
2015-01-10 21:53:33 +0100

    Add the "period" model to the server side

---
+++ api/controllers/PeriodController.js
@@ -0,0 +1,11 @@
+/**
+ * PeriodController
+ *
+ * @description :: Server-side logic for managing periods
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Period.js
@@ -0,0 +1,20 @@
+/**
+* Period.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+	  startDate: {
+		  type: 'date',
+		  required: true
+	  },
+	  endDate: {
+		  type: 'date',
+		  required: true
+	  }
+  }
+};
