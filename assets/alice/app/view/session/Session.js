4220054d0f9da97a1fba650cf0094fc166e351e3
Guillaume Denry <guillaume.denry@gmail.com>
2015-01-25 23:35:48 +0100

    timeslots can be added to sessions

--- assets/alice/app/view/session/Session.js
+++ assets/alice/app/view/session/Session.js
@@ -18,6 +18,16 @@ Ext.define('Alice.view.session.Session', {
 
 	initComponent: function () {
 		var me = this;
+		var timeslotTemplate = Ext.create('Ext.XTemplate',
+										  '{[Ext.Date.format(values.date, "F j, Y")]}, from {[this.toTime(values.startTime)]} to {[this.toTime(values.endTime)]}', {
+											  toTime: function (s) {
+												  var hours = Math.floor(s / 3600);
+												  s %= 3600;
+												  var minutes = Math.floor(s / 60);
+												  var seconds = s % 60;
+												  return hours + ':' + minutes;
+											  }
+										  });
 
 		this.items = [
 			{
@@ -31,16 +41,7 @@ Ext.define('Alice.view.session.Session', {
 					{
 						xtype: 'templatecolumn',
 						text: 'Timeslot',
-						tpl: Ext.create('Ext.XTemplate',
-										'{[Ext.Date.format(values.date, "F j, Y")]}, from {[this.toTime(values.startTime)]} to {[this.toTime(values.endTime)]}', {
-											toTime: function (s) {
-												var hours = Math.floor(s / 3600);
-												s %= 3600;
-												var minutes = Math.floor(s / 60);
-												var seconds = s % 60;
-												return hours + ':' + minutes;
-											}
-										}),
+						tpl: timeslotTemplate,
 						flex: 1
 					}
 				],
@@ -48,7 +49,37 @@ Ext.define('Alice.view.session.Session', {
 					{
 						xtype: 'button',
 						text: 'Add a timeslot to the session',
-						handler: 'addTimeslot'
+						menu: [
+							{text: 'Item 1'},
+							{text: 'Item 2'},
+							{text: 'Item 3'},
+							{text: 'Item 4'}
+						],
+						listeners: {
+							click: function (button) {
+								var menu = button.getMenu();
+								menu.removeAll();
+								Alice.getApplication().getStore('Timeslots').each(function (timeslot) {
+									menu.add({
+										text: timeslotTemplate.applyTemplate(timeslot.data),
+										timeslotId: timeslot.id,
+										handler: function (menu) {
+											console.log('handler');
+											Ext.Ajax.request({
+												url: '/session/' + me.currentSession + '/timeslots/ ' + menu.timeslotId,
+												method: 'POST',
+												success: function () {
+													me.getComponent('timeslots-grid').getStore().load({
+														method: 'GET',
+														url: '/session/' + me.currentSession + '/timeslots'
+													});
+												}
+											});
+										}
+									});
+								});
+							}
+						}
 					},
 					{
 						xtype: 'button',
